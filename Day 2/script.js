5598d66ac22cc54cf2164a2e1d1b5a30daf543f7
Subratacoder <rp6398812@gmail.com>
2025-02-17 23:34:43 +0530

    add day 2 file

---
+++ Day 2/script.js
@@ -0,0 +1,30 @@
+let name = "Subrata";
+let in_Student = true;
+let fav_Programming_Lang = ["javascript", "Python"]
+
+// console.log(name);
+// console.log(in_Student);
+// console.log(fav_Programming_Lang);
+
+let aboutMe = {
+    name: "Subrata",
+    age: 21,
+    isStudnet: true,
+    favLanfg: "Javascript",
+    city: "Kolkata"
+}
+// console.log(aboutMe);
+// console.log(aboutMe.age);
+
+aboutMe.favLanfg = "Python"
+
+// console.log(aboutMe);
+
+
+let myFavHero = ["Ironman", "Superman", "Batman", "Spiderman", "Thor"]
+
+// console.log(myFavHero);
+// console.log(myFavHero[2]);
+myFavHero.push("Deadpool");
+console.log(myFavHero);
+
