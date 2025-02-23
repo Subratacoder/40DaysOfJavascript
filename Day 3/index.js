179b36f297400324f742594ea0190d0472e9c7b0
Subratacoder <rp6398812@gmail.com>
2025-02-23 22:28:54 +0530

    add js and html file

---
+++ Day 3/index.js
@@ -0,0 +1,43 @@
+// Take the number and find if the number is an odd or even number.
+
+const num = 10.5;
+if ((num % 2) == 0) {
+    // console.log(`${num} is even number`);
+    
+}else{
+    // console.log(`${num} is odd number`);
+    
+}
+
+// Let's check if you are eligible to get a driving license. The eligibility to get a driving licence is 18 years.
+
+let age = 20;
+
+let result = age >= 18 ? "Eligible for Lisence" : "Not Eligible For Lisence";
+
+// console.log(result);
+
+//Calculate CTC
+
+let annualSalary = 12300 * 12;
+//20% bonus
+let bonus = annualSalary * 20 / 100 //29520
+let ctc = annualSalary + bonus 
+
+// console.log(`ctc is ${ctc}`);
+
+
+//TRAFFIC LIGHT SIMULATION
+
+// let colour = "GREEN";
+
+// if (colour == "red" || colour == "RED") {
+//     console.log('STOP');
+    
+// }else if(colour == "green" || colour == "GREEN"){
+    
+//     console.log('GO');
+// }
+
+
+//
