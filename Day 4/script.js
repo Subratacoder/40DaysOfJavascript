ffb32d8f42c3c52b5d1a1a1a2159a89acd2b5c57
Subratacoder <rp6398812@gmail.com>
2025-03-07 22:23:45 +0530

    push day 4 assignment

---
+++ Day 4/script.js
@@ -0,0 +1,135 @@
+// Assignment
+
+// let day = "Monday";
+
+// switch (day) {
+//    case "monday":
+//        console.log("It's the start of the week.");
+//        break;
+//    default:
+//        console.log("It's a normal day.");
+// }
+
+//Build a ATM cash withdrawl system
+
+// let amount = 100;
+
+// if (amount <= 100) {
+//     console.log("Withdrawl Successful");
+    
+// }else{
+//     console.log("Invalid Amount");
+    
+// }
+
+//BUILD A CALCULATOR WITH SWITCH CASE
+
+// let num1 = 10 // First Number
+// let num2 =20 // Second Number
+// let operation = "-" //(+, -, *, /, %)
+// let result;
+
+// switch (operation) {
+//     case "+":
+//       result = num1 + num2;  
+//         break;
+//     case "-":
+//         result = num1 - num2;
+//         break;
+//     case "*":
+//         result = num1 * num2;
+//         break;
+//     case "/":
+//         result = num1 / num2;
+
+//     default:
+//         result = "Invalid Operator"
+//         break;
+// }
+
+// console.log(`Result is: ${result}`);
+
+
+//PAY FOR YOUR MOVIE TICKET
+
+// let age = 61
+
+// if (age < 18) {
+//     console.log(`you are children. you have to pay $3`);
+    
+// } else if(age >= 18 && age <= 60) {
+//     console.log(`you are Adults. You have to pay $10`);
+    
+// }else if(age > 60){
+//     console.log(`you are Senior pay $8`);
+    
+// }
+
+//HOROSCOPE SIGN CHECKER
+
+// const month = "March"
+
+// switch (month) {
+//     case "March":
+//     case "April":
+//         console.log("Aries");
+//         break;
+    
+//     case "April":
+//     case "May":
+//         console.log("Taurus");
+//         break;
+
+//     case "May":
+//     case "June":
+//         console.log("Gemini");
+//         break;
+
+//     case "June":
+//     case "July":
+//         console.log("Cancer");
+//         break;
+
+//     case "July":
+//     case "August":
+//         console.log("Leo");
+//         break;
+
+//     case "August":
+//     case "September":
+//         console.log("Virgo");
+//         break;
+
+//     case "September":
+//     case "October":
+//         console.log("Libra");
+//         break;
+
+//     case "October":
+//     case "November":
+//         console.log("Scorpio");
+//         break;
+
+//     default:
+//         console.log("Enter your Birth Month");
+//         break;        
+// }
+
+// let side1 = 30; 
+// let side2 = 20;
+// let side3 = 20;
+
+// if(side1 == side2){
+//     if(side2 == side3){
+//         console.log("Equilateral Triangle");  
+//     }
+// }else if(side1 == side2 || side2 !== side3) {
+//     if (side1 == side3 || side1 !== side2) {
+
+//             console.log("Isosceles Triangle")
+//     }
+        
+// }else if(side1 !== side2 && side2 !== side3 && side1 !== side3) {
+//     console.log("Scalene Triangle");
+    
+// }
